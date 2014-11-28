5e2ac62684dbeede0a07bbfe25eff35b8634434a
Christoph Witzko <github@christophwitzko.com>
2014-11-28 13:33:07 +0100

    feat: implemented tripple semicolon

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 'use strict'
 
+var fixmyjs = require('fixmyjs')
+
 var pkg = require('./package.json')
 
+
+var SEMICOLONS = ';;;'
+var GREEK = ';'
+
+
+function addTrippleSemicolon(data, semicolons){
+  return fixmyjs.fix(data, {asi: false}).replace(/;(\r|\n)+/gm, semicolons + '\n')
+}
+
 module.exports = function(argv) {
-  if (argv.version || argv.v) return console.log(pkg.version)
+  process.stdin.setEncoding('utf8')
+  if(argv.version || argv.v) return console.log(pkg.version)
+  var cgreek = argv.greek || argv.g
+
+  var buffer = ''
+
+  process.stdin.on('readable', function(){
+    var chunk = process.stdin.read()
+    if(chunk !== null) buffer += chunk
+  })
+
+  process.stdin.on('end', function(){
+    process.stdout.write(addTrippleSemicolon(buffer, cgreek ? GREEK : SEMICOLONS))
+  })
 }
