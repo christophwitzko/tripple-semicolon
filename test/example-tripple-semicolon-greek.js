f50304c042c182148c1ee878f3464e2b08c8d4d6
Christoph Witzko <github@christophwitzko.com>
2015-02-16 00:01:27 +0100

    test: added greek mode test

---
+++ test/example-tripple-semicolon-greek.js
@@ -0,0 +1,12 @@
+var app = require('app');
+
+app.use(function(req, res, next) {
+  console.log(req.path);
+  next();
+});
+
+app.all('/', function(req, res) {
+  for (var i = 0; i < 10; i++) {
+    res.send('my cool app ' + i);
+  }
+});
--- test/test.js
+++ test/test.js
@@ -6,9 +6,16 @@ var test = require('tape')
 
 var trippleSemicolon = require('../')
 
-test('tripple-semicolon', function (t) {
+var example = fs.readFileSync(__dirname + '/example.js').toString()
+
+test('tripple-semicolon tripple', function (t) {
   t.plan(1)
-  var example = fs.readFileSync(__dirname + '/example.js').toString()
   var exampleCompare = fs.readFileSync(__dirname + '/example-tripple-semicolon.js').toString()
   t.equal(trippleSemicolon(example), exampleCompare, 'equal')
+})
+
+test('tripple-semicolon greek', function (t) {
+  t.plan(1)
+  var exampleCompare = fs.readFileSync(__dirname + '/example-tripple-semicolon-greek.js').toString()
+  t.equal(trippleSemicolon(example, true), exampleCompare, 'equal')
 })
